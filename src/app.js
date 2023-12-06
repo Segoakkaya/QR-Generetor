f335f1be491e58933337292335295674b0055141
Segoakkaya <akkayasergen59@gmail.com>
2023-12-06 18:05:57 +0300

    end

---
+++ src/app.js
@@ -0,0 +1,7 @@
+//require("../node_modules/jquery/dist/jquery")
+window.$ = $;
+window.jQuery = $;
+jQuery = $;
+    require("../node_modules/bootstrap/dist/js/bootstrap"),
+    require("../node_modules/jquery.qrcode/jquery.qrcode.min"),
+    require("./js/script");
